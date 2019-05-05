697df14b7ee23f27dcda3412f77f00329f85a9d0
kevinwang09 <kevin.wang@sydney.edu.au>
2019-05-05 14:45:00 +1000

    initialise
    initialise

---
+++ install.R
@@ -0,0 +1,23 @@
+# DO NOT EDIT 'install.R'; instead, edit 'install.R.in' and
+# use 'rake' to generate 'install.R'.
+
+if ("BiocManager" %in% rownames(installed.packages()))
+	remove.packages("BiocManager")
+
+install.packages("BiocManager", repos="https://cran.rstudio.com")
+#install.packages("devtools", repos="https://cran.rstudio.com")
+#devtools::install_github("Bioconductor/BiocManager")
+library(BiocManager)
+
+if(BiocManager::version() != "3.9"){
+    BiocManager::install(version="3.9",
+                         update=TRUE, ask=FALSE)
+}
+
+builtins <- c("Matrix", "KernSmooth", "mgcv", "scMerge", "scater")
+
+for (builtin in builtins)
+    if (!suppressWarnings(require(builtin, character.only=TRUE)))
+        suppressWarnings(BiocManager::install(builtin,
+                                              version="3.9",
+                                              update=TRUE, ask=FALSE))
